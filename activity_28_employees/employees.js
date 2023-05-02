f0fd36a6f6b447d02059082ab35e3c7e959d45fa
Zonera Nasir <zkhokhar98@gmail.com>
2023-05-02 10:36:21 -0600

    Committing changes to employees.js

---
+++ activity_28_employees/employees.js
@@ -0,0 +1,59 @@
+```
+    db.employees.insertMany([
+    {
+        name: 'John', 
+        department: 'sales', 
+        projects: ['bluffee', 'jomoorjs', 'auton' ]
+    },
+
+    {
+        name: 'Mary', 
+        department: 'sales', 
+        projects: ['codete', 'auton' ]
+    },
+
+    {
+        name: 'Peter', 
+        department: 'hr', 
+        projects: ['auton', 'zoomblr', 'instory', 'bluffee' ]
+    },
+
+    {
+        name: 'Janet', 
+        department: 'marketing', 
+    },
+
+    {
+        name: 'Sunny', 
+        department: 'marketing', 
+    },
+
+    {
+        name: 'Winter', 
+        department: 'marketing', 
+        projects: [ 'bluffee', 'auton' ]
+    },
+
+    {
+        name: 'Fall', 
+        department: 'marketing', 
+        projects: [ 'bluffee', 'scrosnes' ]
+    },
+
+    {
+        name: 'Summer', 
+        department: 'marketing', 
+    },
+
+    {
+        name: 'Sam', 
+        department: 'marketing', 
+        projects: ['scrosnes' ]
+    },
+
+    {
+        name: 'Maria', 
+        department: 'finances', 
+        projects: ['conix', 'filemenup', 'scrosnes', 'specima', 'bluffee' ]
+    }])
+```
